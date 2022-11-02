a96babfc794a2455d0de92826d5cfbace66ef488
Tri Nguyen <tri.nguyen1086@gmail.com>
2022-11-02 13:32:53 -0700

    fix the test to make it pass after receiving the failing notification from circileci

--- hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
+++ hello-circle/src/main/java/com/trilogyed/hellocircle/controller/HelloCircleController.java
@@ -9,5 +9,11 @@ public class HelloCircleController {
     public String helloCircle(){
         return "Hello, Circle!";
     }
+
+    @GetMapping(value = "/answer")
+    public String getAnswer(){
+        return "The answer is 42!";
+    }
+
 }
 
